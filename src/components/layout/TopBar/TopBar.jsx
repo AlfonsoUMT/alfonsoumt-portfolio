e8569a0b05e8218b3055dcb126b4c04ea775c6ef
Ulises Mendieta <31333323+MendietaU@users.noreply.github.com.>
2026-07-30 10:40:15 -0600

    feat: implement top bar component

--- src/components/layout/TopBar/TopBar.jsx
+++ src/components/layout/TopBar/TopBar.jsx
@@ -1,9 +1,53 @@
-import "./Topbar.css"
-function TopBar(){
-    return(
-        <header className="top-bar">
-            TopBar
+import "./TopBar.css";
+
+function TopBar() {
+    return (
+        <header className="topbar">
+
+            <div className="topbar__left">
+
+                <div className="brand">
+
+                    <code className="brand__icon">
+                        {"<A>"}
+                    </code>
+
+                    <span className="brand__name">
+                        Alfonso UMT
+                    </span>
+
+                </div>
+
+            </div>
+
+            <div className="topbar__center">
+
+                <span className="topbar__file">
+                    about.jsx
+                </span>
+
+            </div>
+
+            <div className="topbar__right">
+
+                <button
+                    className="theme-button"
+                    type="button"
+                    aria-label="Cambiar tema"
+                >
+                    🌙
+                </button>
+
+
+                <code className="window-controls__logo">
+                    {"<A/>"}
+                </code>
+
+
+            </div>
+
         </header>
     );
 }
+
 export default TopBar;
