34dee1bd679974d731d36712c24f84480ba00d9f
Ulises Mendieta <31333323+MendietaU@users.noreply.github.com.>
2026-08-01 00:59:48 -0600

    feat: create file registry

---
+++ src/data/files/index.jsx
@@ -0,0 +1,59 @@
+import { aboutFile } from "./about";
+import { profileFile } from "./profile";
+import { projectsFile } from "./projects";
+import { skillsFile } from "./skills";
+import { contactFile } from "./contact";
+
+export const editorFiles = {
+
+    about: {
+
+        id: "about",
+
+        name: "about.jsx",
+
+        content: aboutFile,
+
+    },
+
+    profile: {
+
+        id: "profile",
+
+        name: "profile.jsx",
+
+        content: profileFile,
+
+    },
+
+    projects: {
+
+        id: "projects",
+
+        name: "projects.jsx",
+
+        content: projectsFile,
+
+    },
+
+    skills: {
+
+        id: "skills",
+
+        name: "skills.jsx",
+
+        content: skillsFile,
+
+    },
+
+    contact: {
+
+        id: "contact",
+
+        name: "contact.jsx",
+
+        content: contactFile,
+
+    }
+
+};
