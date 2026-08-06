3b31e49d69a1bb4d637443381897d298f7e334fa
Ulises Mendieta <31333323+MendietaU@users.noreply.github.com.>
2026-08-06 15:28:50 -0600

    feat: create code statusbar

--- src/components/layout/StatusBar/StatusBar.jsx
+++ src/components/layout/StatusBar/StatusBar.jsx
@@ -1,10 +1,58 @@
-import "./StatusBar.css"
-function StatusBar(){
-    return(
-        <footer className="status-bar">
-            StatusBar
+import "./StatusBar.css";
+
+function StatusBar() {
+
+    return (
+        <footer className="statusbar">
+
+            <div className="statusbar__left">
+
+                <span className="statusbar__item statusbar__item--accent">
+                    ●
+                </span>
+
+                <span className="statusbar__item">
+                    Ready
+                </span>
+
+                <span className="statusbar__separator">
+                    |
+                </span>
+
+                <span className="statusbar__item">
+                    AlfonsoUMT
+                </span>
+
+            </div>
+
+
+            <div className="statusbar__right">
+
+                <span className="statusbar__item">
+                    JavaScript
+                </span>
+
+                <span className="statusbar__separator">
+                    |
+                </span>
+
+                <span className="statusbar__item">
+                    UTF-8
+                </span>
+
+                <span className="statusbar__separator">
+                    |
+                </span>
+
+                <span className="statusbar__item">
+                    LF
+                </span>
+
+            </div>
+
         </footer>
     );
+
 }
 
 export default StatusBar;
